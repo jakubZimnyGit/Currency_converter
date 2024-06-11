f5ffdfc2ca663593e7031e89363532b7125e5bc5
Jakub Zimny <jakub.zimny.gdansk@gmail.com>
2024-06-11 15:41:19 +0200

    created function to convert currencies using currency exchange rate API

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ load_dotenv()
 printer = PrettyPrinter()
 
 API_KEY = os.getenv('API_KEY')
-URL = f'https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}'
+URL = f'https://api.freecurrencyapi.com/v1/currencies?apikey={API_KEY}'
 
 def get_currencies():
 
@@ -20,13 +20,31 @@ def get_currencies():
     return response
 
 def display_currencies(data):
-    for a in data:
-        print(a[0])
 
+    for a in data:
+        currency_data = a[1]
+        short = a[0]
+        symbol = currency_data['symbol']
+        name = currency_data['name']
+        print(f'{short} - {name} - {symbol}')
+
+def convert_currencies(base_currnecy, currency, amount):
+    convert_url = f'https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}&base_currency={base_currnecy}&currencies={currency}'
+    response = requests.get(convert_url).json()
+    printer.pprint(response)
+    rate = response['data']['EUR']
+    result: float = amount * rate
+    print(f'{amount} {base_currnecy} ==> {round(result, 2)} {currency}')
+
+def currency_converter():
+    base_currency = input('enter base currency shortcut (E.g USD): ')
+    amount = float(input('enter the amonut of money in that currency: '))
+    currency_to_convert_to = input('enter the currency to convert to: ')
+    return convert_currencies(base_currency, currency_to_convert_to, amount)
 
 def main():
-    data = get_currencies()
-    display_currencies(data)
+    currency_converter()
+
 
 if __name__ == '__main__':
     main()
