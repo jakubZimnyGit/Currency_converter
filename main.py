70980974b12bd1da45cc3f86b4d44496ab1710eb
Jakub Zimny <jakub.zimny.gdansk@gmail.com>
2024-06-12 13:53:27 +0200

    refactoring the code

--- main.py
+++ main.py
@@ -5,19 +5,18 @@ from pprint import PrettyPrinter
 
 load_dotenv()
 
+Base_url = 'https://api.freecurrencyapi.com/v1/'
 printer = PrettyPrinter()
-
 API_KEY = os.getenv('API_KEY')
-URL = f'https://api.freecurrencyapi.com/v1/currencies?apikey={API_KEY}'
+
 
 def get_currencies():
-
-    response = requests.get(URL).json()['data']
-
+    url = f'{Base_url}currencies?apikey={API_KEY}'
+    response = requests.get(url).json()['data']
     response = list(response.items())
-
     response.sort()
     return response
+
 
 def display_currencies():
     data = get_currencies()
@@ -29,16 +28,21 @@ def display_currencies():
         name = currency_data['name']
         print(f'{short} - {name} - {symbol}')
     print('\n\n')
+
+
 def get_user_currency_input():
     base_currency = input('enter base currency shortcut (E.g USD): ')
     currency_to_convert_to = input('enter the currency to convert to: ')
     return base_currency, currency_to_convert_to
 
+
 def get_exchange_rate(base_currency, currency):
-    convert_url = f'https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}&base_currency={base_currency}&currencies={currency}'
-    response = requests.get(convert_url).json()
+    url = f'{Base_url}latest?apikey={API_KEY}&base_currency={base_currency}&currencies={currency}'
+    response = requests.get(url).json()
     rate = response['data'][currency]
     return rate
+
+
 def convert_currencies(base_currency, currency, amount):
     rate = get_exchange_rate(base_currency=base_currency, currency=currency)
     result: float = amount * rate
@@ -50,11 +54,24 @@ def currency_converter():
     amount = float(input('enter the amount of money in base currency: '))
     return convert_currencies(base_currency, currency_to_convert_to, amount)
 
+
+def display_exchange_rate():
+    base_currency, currency = get_user_currency_input()
+    rate = get_exchange_rate(base_currency, currency)
+    print(f'\n\n{base_currency} ---> {currency} exchange rate: {rate}\n\n')
+
+
+def option_select():
+    print('------Currency Converter--------')
+    print('1. List of all currencies\n2. Convert currency\n3. currency exchange rate\nq - quit')
+    option = input('Choose option (1-3): ')
+    return option
+
+
 def menu():
     while True:
-        print('------Currency Converter--------')
-        print('1. List of all currencies\n2. Convert currency\n3. currency exchange rate\nq - quit')
-        option = input('Choose option (1-3): ')
+
+        option = option_select()
         match option:
             case "q":
                 break
@@ -65,13 +82,12 @@ def menu():
                 currency_converter()
                 pass
             case "3":
-                base_currency, currency = get_user_currency_input()
-                rate = get_exchange_rate(base_currency, currency)
-                print(f'\n\n{base_currency} ---> {currency} exchange rate: {rate}\n\n')
+                display_exchange_rate()
+
+
 def main():
     menu()
 
 
 if __name__ == '__main__':
     main()
-
